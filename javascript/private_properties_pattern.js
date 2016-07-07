5426741398d14e483770d40c3dc8ebda7fe2cf59
Ben Babtsov <ben.babtsov@ufl.edu>
2016-07-07 00:44:34 -0400

    Update and rename javascrpt/private_properties_pattern.js to javascript/private_properties_pattern.js

--- javascript/private_properties_pattern.js
+++ javascript/private_properties_pattern.js
@@ -1,10 +1,10 @@
 "use strict";
 
 // Arrange
-function Qaz(name, val) {
+function Qaz(name, val, secretString) {
     
-    // private properties
-    var privateString = "hidden";
+    // private fields
+    var privateString = secretString;
     var privateArray = [];
     // define our own prototype
     Qaz.prototype = {
@@ -21,7 +21,7 @@ function Qaz(name, val) {
     }
     // set the object instance's prototype to Qaz.prototype
     var self = Object.create(Qaz.prototype);
-    // set public properties
+    // set public fields
     self.name = name;
     self.val = val;
     return self;
@@ -29,7 +29,7 @@ function Qaz(name, val) {
 }
 
 // Act
-var qaz = new Qaz("judge",9); // can also be created without the "new" keyword
+var qaz = new Qaz("judge",9,"hidden"); // can also be created without the "new" keyword
 
 // Assert
 console.assert(qaz.getPrivateString() === "hidden");
