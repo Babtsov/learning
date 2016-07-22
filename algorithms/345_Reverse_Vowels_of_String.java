5d48db393b9d66601358b123cb9a88b75823522b
Ben Babtsov <ben.babtsov@ufl.edu>
2016-07-21 22:36:22 -0400

    Create 345_Reverse_Vowels_of_String.java

---
+++ algorithms/345_Reverse_Vowels_of_String.java
@@ -0,0 +1,18 @@
+public String reverseVowels(String s) {
+    char[] arr = s.toCharArray();
+    int left = 0, right = s.length() - 1;
+    while (left < right) {
+        if (!isVowel(arr[left])) {
+            left++;
+        } else if (!isVowel(arr[right])) {
+            right--;
+        } else {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    return new String(arr);
+}
