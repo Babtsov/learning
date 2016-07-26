336d36dcf620ef12c360b3280140bc8ac5a359fe
Ben Babtsov <ben.babtsov@ufl.edu>
2016-07-26 00:07:44 -0400

    Update 278_First_Bad_Version.java

--- algorithms/278_First_Bad_Version.java
+++ algorithms/278_First_Bad_Version.java
@@ -1,6 +1,7 @@
+// iterative strategy
 public int firstBadVersion(int n) {
     int begin = 1, end = n;
-    while (end >= begin) {
+    while (end >= begin) { 
         int middle = (end - begin) / 2 + begin;
         if (isBadVersion(middle)) {
             if (end - begin <= 1) { 
@@ -11,5 +12,25 @@ public int firstBadVersion(int n) {
             begin = middle + 1;
         }
     }
-    return -1;
+    return -1; // optional:  return -1 if no bad version exists
+}
+
+// recursive strategy
+public int firstBadVersion(int n) {
+    return firstBadVersionRecursive(1, n, n);
+}
+
+public int firstBadVersionRecursive(int begin, int end, int n) {
+    if (end < begin) { // optional:  return -1 if no bad version exists
+        return -1;
+    }
+    int middle = (end - begin) / 2 + begin;
+    if (isBadVersion(middle)) {
+        if (end - begin <= 1) { 
+            return middle;
+        }
+        return firstBadVersionRecursive(begin, middle, n);
+    }else {
+        return firstBadVersionRecursive(middle + 1, end, n);
+    }
 }
