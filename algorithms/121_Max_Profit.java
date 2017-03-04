bb9b99817c520a741b5b3d9242dbaaea63d37a5b
Ben Babtsov <ben.babtsov@ufl.edu>
2017-03-03 21:02:36 -0500

    Update 121_Max_Profit.java

--- algorithms/121_Max_Profit.java
+++ algorithms/121_Max_Profit.java
@@ -20,3 +20,13 @@ public int maxProfit(int[] prices) {
     }
     return diff;
 }
+// here is a nicer version:
+public int maxProfit(int[] prices) {
+    int minPrice = Integer.MAX_VALUE;
+    int maxProfit = 0;
+    for (int i = 0; i < prices.length; i++) {
+        minPrice = Math.min(minPrice, prices[i]);
+        maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    }
+    return maxProfit;
+}
