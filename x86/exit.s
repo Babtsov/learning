74a0ffd95bce7cad1a7e99bc5bfd6f686f2bdb05
Ben Babtsov <ben.babtsov@ufl.edu>
2017-11-08 00:06:48 -0800

    Update exit.s

--- x86/exit.s
+++ x86/exit.s
@@ -1,7 +1,9 @@
+// as exit.s -o exit.o
+// ld exit.o -o exit
 .section .data  
 .section .text
-.globl _start  # assembler shouldn’t discard this symbol after assembly, because the linker will need it
+.globl _start  // assembler shouldn’t discard this symbol after assembly, because the linker will need it (ld will complain otherwise)
 _start:
-movl $1, %eax  # syscall number for exit()
-movl $0, %ebx  # argument to exit
-int $0x80  # execute the syscall
+movl $1, %eax  // syscall number for exit()
+movl $0, %ebx  // argument to exit
+int $0x80  // execute the syscall
