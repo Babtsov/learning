87dbacd0092f5a660f99433b08974a781d55241e
Ben Babtsov <ben.babtsov@ufl.edu>
2018-08-22 20:21:34 -0700

    Create centos7_install.sh

---
+++ tmux/centos7_install.sh
@@ -0,0 +1,24 @@
+# script to help installing tmux on centos 7
+# src: https://gist.github.com/pierreprinetti/86fbcd28e5cc1b3e0b9b762e4c12fad2
+
+# remove old pkgs
+sudo yum remove libevent libevent-devel libevent-headers
+
+# install ncurses
+sudo yum install ncurses-devel
+
+# download libevent src
+cd /usr/local/src
+sudo curl -L https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz -o libevent-2.0.21-stable.tar.gz
+sudo tar xvzf libevent-2.0.21-stable.tar.gz
+cd libevent-2.0.21-stable
+sudo ./configure && sudo make
+sudo make install
+sudo ln -s /usr/local/lib/libevent-2.0.so.5 /usr/lib64/libevent-2.0.so.5
+
+cd /usr/local/src
+sudo curl -L https://github.com/tmux/tmux/archive/2.5.tar.gz -o tmux-2.5.tar.gz
+sudo tar -xvzf tmux-2.5.tar.gz
+cd tmux-2.5
+sudo ./configure && sudo make
+sudo make install
